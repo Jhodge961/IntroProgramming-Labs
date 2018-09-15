260b58f5f2b3444590bf4a88b1b97811bd418f96
Jason Hodge <jason.hodge1@marist.edu>
2018-09-15 01:26:09 -0700

    Solution to Mad Lib exercise.

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#MadLib Program
+
+    def madlib():
+	noun = input ("Enter a noun")
+	verb = input ("Enter a verb")
+	adjective = input ("Enter a an adjective")
+	place = input ("Enter a place")
+	print ("In the " + place + "we saw a " + adjective + noun + verb "!")
+	
+madlib()
