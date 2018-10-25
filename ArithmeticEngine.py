e38c417887c7ca1432028e05b84c7dbe80464f15
Jason Hodge <jason.hodge1@marist.edu>
2018-10-25 11:51:06 -0700

    Handling Errors.

---
+++ ArithmeticEngine.py
@@ -0,0 +1,41 @@
+# CMPT 120 - Lab #6
+# Jason Hodge
+# 25 Oct 2018
+###
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+
+def doLoop():
+     while True:
+         cmd = input("What computation do you want to perform? ").lower()
+         if cmd == "quit":
+             break
+         else: 
+                 num1 = int(input("Enter the first number: "))
+                 num2 = int(input("Enter the second number: "))
+                 
+                 if cmd == "add":
+                     result = num1 + num2
+                 elif cmd == "sub":
+                     result = num1 - num2
+                 elif cmd == "mult":
+                     result = num1 * num2
+                 elif cmd == "div":
+                     result = num1 / num2
+                 elif cmd == "quit":
+                     break
+    
+                 print("The result is " + str(result) + ".\n")
+ 
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+main()
