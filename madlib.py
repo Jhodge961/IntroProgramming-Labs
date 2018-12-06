726ac3c2366d013fe6838c93fb917c7207cd16f5
Jason Hodge <jason.hodge1@marist.edu>
2018-12-06 14:14:31 -0500

    Solution to Mad Libs exercise.

--- madlib.py
+++ madlib.py
@@ -1,10 +1,17 @@
 #MadLib Program
 
-    def madlib():
-	noun = input ("Enter a noun")
-	verb = input ("Enter a verb")
-	adjective = input ("Enter a an adjective")
-	place = input ("Enter a place")
-	print ("In the " + place + "we saw a " + adjective + noun + verb "!")
-	
-madlib()
+def madLib():
+    global noun, verb, adjective, place
+    noun = input ("Enter a noun: ")
+    verb = input ("Enter a verb: ")
+    adjective = input ("Enter a an adjective: ")
+    place = input ("Enter a place: ")
+
+def makeAndPrintSentence():
+    print ("In the " + place + "we saw a " + adjective + noun + verb + "!")
+
+def main():
+    madLib()
+    makeAndPrintSentence()
+    
+main()
